48eb12f1ef96c051ab43a750efd7602cd2a9a4e3
Ron0Studios <rudrrayan@gmail.com>
2023-02-23 12:46:02 +0000

    client/server communication working
    added CORS to expressjs and now the two localhosts can communicate!

--- client/src/judge/index.js
+++ client/src/judge/index.js
@@ -1,10 +1,24 @@
 
-const runProgram = (code, language) => {
+const runProgram = async (code, language) => {
     console.log(code)
     console.log(language)
 
     switch (language) {
         case "py3":
+            //const resjson = await res.json().catch(err=>console.log(err))
+            
+            const res = await fetch('http://localhost:5000/submit', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({hi:"hey"})
+            })
+            .then(response => response.json())
+            .then(response => console.log(JSON.stringify(response)))
+            //.then(resp => console.log(resp))
+            
             break;
     
         default:
--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,26 @@
 const express = require('express')
+const bp = require('body-parser')
+const cors = require('cors')
 const app = express()
+
+app.use(bp.json())
+app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+
 
 
 app.get("/api", (req, res) => {
+    //res.send("hello there")
     res.json({"users": ["userOne", "userTwo", "ron0studios"]})
 })
 
 
-app.post("/submissions", (req,res) => {
-    console.log(req)
-    res.send("YOOOOO");
+app.post("/submit", (req,res) => {
+    
+    console.log("received")
+    console.log(req.body)
+    //res.send("hello there!")
+    res.json({hi:2})
 })
 
 app.listen(5000, () => {console.log("server started on port 5000 :D")})
